e6916322eaaa8eff40d6b98e485e91a8f16dece3
Pancakem <pancakesdeath@protonmail.com>
2019-07-15 14:31:45 +0300

    do arrangement of date

--- Clean/datefilter.go
+++ Clean/datefilter.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"math/rand"
 	"strings"
 	"time"
 )
@@ -10,13 +11,13 @@ import (
 // in format from instagram `2019-05-12T17:29:54.000Z`- no need to format
 func formatTimeStamp(timestamp string) string {
 	//append .000Z to the end
-	timestamp += ".000Z"
+	timestamp += "Z"
 	newString := strings.Replace(timestamp, " ", "T", 1)
 	return newString
 }
 
 func getTimeFromStamp(timestamp string) *time.Time {
-	timeValue, err := time.Parse("2019-05-12T17:29:54.000Z", timestamp)
+	timeValue, err := time.Parse(time.RFC3339, timestamp)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -33,4 +34,69 @@ func isInLimit(limits []string, timestamp string) bool {
 
 	// if the timestamp is within the limits
 	return theTime >= lowerLimit && theTime <= upperLimit
+}
+
+// create an order in the tweets
+func createOrder(records [][]string) [][]string {
+	return records
+}
+
+// quicksort the timestamps into and ascending order
+func quickSort(slice [][]string) [][]string {
+
+	timestampIndex := 5
+	length := len(slice)
+
+	if length <= 1 {
+		sliceCopy := make([][]string, length)
+		copy(sliceCopy, slice)
+		return sliceCopy
+	}
+
+	m := slice[rand.Intn(length)]
+
+	less := make([][]string, 0, length)
+	middle := make([][]string, 0, length)
+	more := make([][]string, 0, length)
+
+	for _, item := range slice {
+		switch {
+		case topCompare(item[timestampIndex], m[timestampIndex], "lesser"): //item < m:
+			less = append(less, item)
+		case topCompare(item[timestampIndex], m[timestampIndex], "equal"): //item < m:
+			middle = append(middle, item)
+		case topCompare(item[timestampIndex], m[timestampIndex], "greater"): // item > m:
+			more = append(more, item)
+		}
+	}
+
+	less, more = quickSort(less), quickSort(more)
+
+	less = append(less, middle...)
+	less = append(less, more...)
+
+	return less
+}
+
+// compares the times
+// if time1 is greater it returns a true
+// false otherwise
+func compare(time1, time2 *time.Time, ty string) bool {
+	switch ty {
+	case "equal":
+		return time1.Unix() == time2.Unix()
+	case "greater":
+		return time1.Unix() > time2.Unix()
+	case "lesser":
+		return time1.Unix() < time2.Unix()
+	default:
+		return false
+	}
+}
+
+// wrap compare
+func topCompare(timestamp1, timestamp2 string, ty string) bool {
+	time1 := getTimeFromStamp(formatTimeStamp(timestamp1))
+	time2 := getTimeFromStamp(formatTimeStamp(timestamp2))
+	return compare(time1, time2, ty)
 }
